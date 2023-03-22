01b4052353919e1eab8170af72c88b03f971ef94
FuuKowatty <miedzio.2000@gmail.com>
2023-03-22 17:39:27 +0100

    basic folder structure

---
+++ src/styles/Container.styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    height: 500px;
+    width: 1200px;
+    max-width: 100%;
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    colors: {
+        primary: '#EEEEEE',
+        secondary: '#393E46',
+        bodyBg: '#001A38',
+        darkerBlue: '#00224E',
+        darkBlue: '#003367',
+        standsOut: '#FAD61D',
+    },
+    fonts: {
+
+    }
+}
