a57bcc29b30b8be22216c164cae852bf6e955d60
FuuKowatty <miedzio.2000@gmail.com>
2023-03-25 10:54:15 +0100

    reset spacing and add colors, background, font

---
+++ src/styles/Global.ts
@@ -0,0 +1,17 @@
+import { Theme } from 'styles/Theme';
+import { createGlobalStyle } from 'styled-components';
+
+
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    body {
+        color: ${Theme.colors.primary};
+        background: ${Theme.colors.bodyBg};
+        font-family: 'Roboto', sans-serif;
+    }
+`;
