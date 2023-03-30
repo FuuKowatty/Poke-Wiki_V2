b397f3e0fe0038f935c11c93a7e555c010068ef0
FuuKowatty <miedzio.2000@gmail.com>
2023-03-30 09:59:31 +0200

    Add App Provider to context and Navbar to render

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import Home from 'pages/Home';
-import { Container } from 'styles/Container.styled';
+import {Home} from 'pages/Home';
+import { Container } from 'styles/StyledContainer';
+import { GlobalStyles } from 'styles/Global';
+import { Navbar } from 'layouts/Navbar';
+import { AppProvider } from 'context';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
-
-function App() {
+export function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
+      <AppProvider>
       <Container>
-        {/* <Navbar /> */}
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Container>
+      </AppProvider>
     </BrowserRouter>
   );
 }
-
-export default App
