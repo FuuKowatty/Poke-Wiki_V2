be697128d363b5373cb7f7c9c7223be816d50de7
FuuKowatty <miedzio.2000@gmail.com>
2023-03-30 09:53:01 +0200

    set layout of Banner Element

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,23 @@
-import { BannerButton, BannerContainer, BannerContent, BannerDescription, BannerImage, BannerTitle } from 'styles/Banner';
+import { BannerButton, BannerContainer, BannerContent, BannerDescription, BannerImage, BannerTitle, BannerTitleText1, BannerTitleText2 } from 'styles/Banner';
+import banner from 'assets/banner2.png';
+import bannerMobile from 'assets/bannerMobile.png';
+import { useAppContext } from 'hooks/useAppContext';
 
 export function Banner() {
+
+    const isMobile = useAppContext()
+
     return (
         <BannerContainer>
-          <BannerImage alt="banner-image" />
+          <BannerImage src={isMobile ? bannerMobile : banner} alt="banner-image" />
           <BannerContent>
-            <BannerTitle>POKEMONS</BannerTitle>
+            <BannerTitle>
+              <BannerTitleText1>THE WIKIPEDIA OF</BannerTitleText1>
+              <BannerTitleText2>POKEMONS</BannerTitleText2>
+            </BannerTitle>
             <BannerDescription>
-              POKEWIKI is library of pokemons based on pokeAPI. It enable you acces
-              you to list of all pokemons, their specific info or some curio.
+              POKEWIKI is library of pokemons based on pokeAPI. It enable you access
+              you to list of all pokemons, their specific info or some fun fact.
             </BannerDescription>
     
             <BannerButton>Get Started</BannerButton>
