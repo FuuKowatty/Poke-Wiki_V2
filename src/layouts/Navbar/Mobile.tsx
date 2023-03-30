2ba4af104545c2a30dd867217b08d7cb411c5211
FuuKowatty <miedzio.2000@gmail.com>
2023-03-30 11:46:55 +0200

    add mobile searchbar toggling function

--- src/layouts/Navbar/Mobile.tsx
+++ src/layouts/Navbar/Mobile.tsx
@@ -1,19 +1,35 @@
-import { Logo } from 'components/Logo';
-import { IconButton } from 'styles/StyledContainer';
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {AiOutlineSearch} from 'react-icons/ai'
+import { Searchbar } from './Searchbar'
+import { Logo } from 'components/Logo'
+import { IconButton } from 'styles/StyledContainer'
+import { useAppContext } from 'hooks/useAppContext'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineSearch } from 'react-icons/ai'
+import {BiArrowBack} from 'react-icons/bi'
+
 
 export function Mobile() {
+  const { toggleMobileSearchbar, isMobileSearchbarToggled } = useAppContext()
 
   return (
     <>
-      <IconButton>
-        <GiHamburgerMenu />
-      </IconButton>
-      <Logo />
-      <IconButton>
-        <AiOutlineSearch />
-      </IconButton>
+      {isMobileSearchbarToggled ? (
+        <>
+        <IconButton onClick={() => toggleMobileSearchbar()}>
+          <BiArrowBack />
+        </IconButton>
+        <Searchbar />
+        </>
+      ) : (
+        <>
+          <IconButton>
+            <GiHamburgerMenu />
+          </IconButton>
+          <Logo />
+          <IconButton onClick={() => toggleMobileSearchbar()}>
+            <AiOutlineSearch />
+          </IconButton>
+        </>
+      )}
     </>
   )
 }
