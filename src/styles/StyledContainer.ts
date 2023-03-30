b656c993820792617dc41ecf8105ea1affe09fb2
FuuKowatty <miedzio.2000@gmail.com>
2023-03-30 09:57:40 +0200

    set more width, center element and add IconButton, SearchIcon styled components

--- src/styles/StyledContainer.ts
+++ src/styles/StyledContainer.ts
@@ -1,7 +1,20 @@
+import { Theme } from './Theme'
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 1440px;
     max-width: 100%;
+`
 
+export const IconButton = styled.button`
+    cursor: pointer;
+    border: none;
+    background: none;
+    color: ${Theme.colors.primary};
+    font-size: ${Theme.fonts.headerThree};
+`
+export const SearchIcon = styled(IconButton)`
+    font-size: ${Theme.fonts.normal};
 `
