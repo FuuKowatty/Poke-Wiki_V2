30e813cb2e23a437139dc0b56d4c3d13a028a8e2
FuuKowatty <miedzio.2000@gmail.com>
2023-03-31 20:00:34 +0200

    added react spring animation

--- src/layouts/Navbar/Mobile.tsx
+++ src/layouts/Navbar/Mobile.tsx
@@ -2,26 +2,47 @@ import { Searchbar } from './Searchbar'
 import { Logo } from 'components/Logo'
 import { IconButton } from 'styles/StyledContainer'
 import { useAppContext } from 'hooks/useAppContext'
+import { StyledDrawer } from 'styles/Drawer'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { AiOutlineSearch } from 'react-icons/ai'
-import {BiArrowBack} from 'react-icons/bi'
-
+import { BiArrowBack } from 'react-icons/bi'
+import { IoMdClose } from 'react-icons/io'
+import { useTransition } from '@react-spring/web'
+import { useState } from 'react'
 
 export function Mobile() {
-  const { toggleMobileSearchbar, isMobileSearchbarToggled } = useAppContext()
+  const { browserWidth } = useAppContext()
+
+  const [isMobileSearchbarToggled, setIsMobileSearchbarToggled] = useState(false)
+  const [isDrawerToggled, setIsDrawerToggled] = useState(false)
+
+  const toggleMobileSearchbar = () => {
+    console.log(isMobileSearchbarToggled)
+    setIsMobileSearchbarToggled((prev) => !prev)
+  }
+
+  const toggleDrawer = () => {
+    setIsDrawerToggled((prev) => !prev)
+  }
+
+  const transition = useTransition(isDrawerToggled, {
+    from: { x: -browserWidth },
+    enter: { x: 0 },
+    leave: { x: -browserWidth },
+  })
 
   return (
     <>
       {isMobileSearchbarToggled ? (
         <>
-        <IconButton onClick={() => toggleMobileSearchbar()}>
-          <BiArrowBack />
-        </IconButton>
-        <Searchbar />
+          <IconButton onClick={() => toggleMobileSearchbar()}>
+            <BiArrowBack />
+          </IconButton>
+          <Searchbar />
         </>
       ) : (
         <>
-          <IconButton>
+          <IconButton onClick={() => toggleDrawer()}>
             <GiHamburgerMenu />
           </IconButton>
           <Logo />
@@ -29,6 +50,17 @@ export function Mobile() {
             <AiOutlineSearch />
           </IconButton>
         </>
+      )}
+      {transition((style, item) =>
+        item ? (
+          <StyledDrawer style={style}>
+            <IconButton onClick={() => toggleDrawer()}>
+              <IoMdClose />
+            </IconButton>
+          </StyledDrawer>
+        ) : (
+          false
+        ),
       )}
     </>
   )
