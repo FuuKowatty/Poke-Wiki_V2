cc5bc1e08be5263dddfd108924d51262a0bb5c79
FuuKowatty <miedzio.2000@gmail.com>
2023-04-19 18:21:26 +0200

    created new icons and change some media queries

--- src/styles/Navbar/StyledNavbar.ts
+++ src/styles/Navbar/StyledNavbar.ts
@@ -1,47 +1,61 @@
 import { Theme } from 'styles/Theme'
+import { device } from 'styles/Breakpoints'
+import { IconButton } from 'styles/StyledContainer'
 import styled from 'styled-components'
 
-interface Props {
-    isMobile: boolean
-}
+export const StyledNavbar = styled.div`
+  min-height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: ${Theme.spacing[2]} ${Theme.spacing[4]};
 
-export const StyledNavbar = styled.div<Props>`
-    display: flex;
+  @media ${device.tablet} {
+    padding: ${Theme.spacing[4]};
     justify-content: space-between;
-    align-items: center;
-    padding: ${(props) => props.isMobile ? Theme.spacing[2] : Theme.spacing[4]}
-    
+  }
 `
 
 export const StyledLogo = styled.div`
-    font-weight: 900;
-    font-family: 'Golos Text', sans-serif;
-    font-size: ${Theme.fonts.headerTwo};
+  font-weight: 900;
+  font-family: 'Golos Text', sans-serif;
+  font-size: ${Theme.fonts.headerTwo};
+`
+
+export const StyledLogoImage = styled.img`
+  width: 48px;
+  height: 48px;
 `
 
 export const StyledSearch = styled.form`
-    position: relative;
-    align-items: center;
-    justify-content: space-between;
-    background: ${Theme.colors.primary};
-    display: flex;
-    border-radius: 4px;
-    color: ${Theme.colors.secondary};
-    padding: 0 ${Theme.spacing[2]}
+  position: relative;
+  display: flex;
+  min-width: 275px;
+  gap: ${Theme.spacing[1]};
+  height: 35px;
 `
 
 export const StyledInput = styled.input`
-    padding: ${Theme.spacing[2]};
-    color: ${Theme.colors.secondary};
-    width: 100%;
-    height: 100%;
-    background: none;
-    border: none;
-    fonr-size: ${Theme.fonts.normal};
-    outline: none;
-    opacity: .5;
+  padding: ${Theme.spacing[2]};
+  color: ${Theme.colors.secondary};
+  width: 100%;
+  height: 100%;
+  background: none;
+  border: none;
+  background: ${Theme.colors.primary};
+  border-top-left-radius: ${Theme.spacing[1]};
+  border-bottom-left-radius: ${Theme.spacing[1]};
+`
 
-    &:focus {
-        opacity: 1;
-    }
+export const SearchIcon = styled(IconButton)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: ${Theme.fonts.headerFour};
+  padding: 0 ${Theme.spacing[2]};
+  background: ${Theme.colors.standsOut};
+  height: 100%;
+  color: ${Theme.colors.darkerBlue};
+  border-top-right-radius: ${Theme.spacing[1]};
+  border-bottom-right-radius: ${Theme.spacing[1]};
 `
