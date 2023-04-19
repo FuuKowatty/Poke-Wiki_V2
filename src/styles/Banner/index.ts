be6a97c124f7b9dc2b9ef9e68c041379bb0a9c15
FuuKowatty <miedzio.2000@gmail.com>
2023-04-19 18:20:19 +0200

    add more responsive Banner

--- src/styles/Banner/index.ts
+++ src/styles/Banner/index.ts
@@ -1,146 +1,140 @@
-import { Theme } from 'styles/Theme';
-import { device } from 'styles/Breakpoints';
-import styled from 'styled-components';
+import { Theme } from 'styles/Theme'
+import { device } from 'styles/Breakpoints'
+import styled from 'styled-components'
 
 export const BannerContainer = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100%;
-    flex-direction: column;
-    background: -webkit-linear-gradient(45deg, #003367,#123049);/* Chrome 10-25, Safari 5.1-6 */                          background: linear-gradient(45deg, #003367,#123049);/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */                                             ;
-    
+  display: flex;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
+  background: ${Theme.colors.bannerColor};
 
-    @media ${device.tablet} {
-        flex-direction: row;
-        justify-content: center;        
-    }
+  @media ${device.tablet} {
+    flex-direction: row;
+    justify-content: center;
+  }
 
-    @media ${device.desktop} {
-        gap: ${Theme.spacing[3]}
-    }
+  @media ${device.desktop} {
+    gap: ${Theme.spacing[3]};
+  }
 `
 export const BannerContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: 420px;
-    width: 100%;
-    padding: ${Theme.spacing[3]} 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 420px;
+  width: 100%;
+  padding: ${Theme.spacing[3]} 0;
 
-    @media ${device.tablet} {
-        align-items: start;
-        max-width: 260px;
-    }
+  @media ${device.tablet} {
+    align-items: start;
+    max-width: 260px;
+  }
 
-    @media ${device.tablet} {
-        max-width: 350px;
-    }
+  @media ${device.laptop} {
+    max-width: 350px;
+  }
 `
 
 export const BannerImage = styled.img`
-    max-width: 350px;
-    width: 90%;
-    padding-top: ${Theme.spacing[3]};
+  max-width: 350px;
+  width: 90%;
+  padding-top: ${Theme.spacing[3]};
 
-    @media ${device.tablet} {
-        padding-top: 0;
-    }
+  @media ${device.tablet} {
+    padding-top: 0;
+  }
 
-    @media ${device.laptop} {
-        max-width: 450px;
-    }
+  @media ${device.laptop} {
+    max-width: 450px;
+  }
 
-    @media ${device.desktop} {
-        max-width: 600px;
-    }
+  @media ${device.desktop} {
+    max-width: 600px;
+  }
 `
 export const BannerTitle = styled.div`
-    text-align: center;
-    font-weight: bold;
-    font-family: Golos Text;
-    width: 100%;
-    line-height: 0.75;
+  text-align: center;
+  font-weight: bold;
+  font-family: Golos Text;
+  width: 100%;
+  line-height: 0.75;
 
-    @media ${device.tablet} {
-        text-align: left;
-    }
+  @media ${device.tablet} {
+    text-align: left;
+  }
 
-    @media ${device.laptop} {
-        
-    }
-    
+  @media ${device.laptop} {
+  }
 `
 
 export const BannerTitleText1 = styled.span`
-    color: ${Theme.colors.standsOut};
-    font-size: ${Theme.fonts.headerThree};
-    display: block;
+  color: ${Theme.colors.standsOut};
+  font-size: ${Theme.fonts.headerThree};
+  display: block;
 
-    @media ${device.laptop} {
-        font-size: ${Theme.fonts.headerTwo}   
-    }
+  @media ${device.laptop} {
+    font-size: ${Theme.fonts.headerTwo};
+  }
 `
 
 export const BannerTitleText2 = styled.span`
-    color: ${Theme.colors.primary};
-    font-size: ${Theme.fonts.headerOne};
+  color: ${Theme.colors.primary};
+  font-size: ${Theme.fonts.headerOne};
 
-    @media ${device.laptop} {
-        font-size: ${Theme.fonts.headerPlus}
-    }
+  @media ${device.laptop} {
+    font-size: ${Theme.fonts.headerPlus};
+  }
 `
 
 export const BannerDescription = styled.div`
-    line-height: ${Theme.fonts.normal};
-    padding: ${Theme.spacing[3]} ${Theme.spacing[3]};
-    text-align: left;
+  line-height: ${Theme.fonts.normal};
+  padding: ${Theme.spacing[3]} ${Theme.spacing[3]};
+  text-align: left;
 
-    @media ${device.tablet} {
-        padding: ${Theme.spacing[3]} 0;
-        max-width: 260px;
-    }
+  @media ${device.tablet} {
+    padding: ${Theme.spacing[3]} 0;
+    max-width: 260px;
+  }
 
-    @media ${device.laptop} {
-        font-size: ${Theme.fonts.headerFour};
-        max-width: 350px;
-    }
+  @media ${device.laptop} {
+    font-size: ${Theme.fonts.headerFour};
+    max-width: 350px;
+  }
 `
 
 export const BannerButton = styled.button`
-    width: 90%;
-    cursor: pointer;
-    background: ${Theme.colors.standsOut};
-    border: none;
+  width: 90%;
+  cursor: pointer;
+  background: ${Theme.colors.standsOut};
+  border: none;
+  padding: ${Theme.spacing[3]};
+  color: ${Theme.colors.darkBlue};
+  font-weight: bold;
+  font-size: ${Theme.fonts.normal};
+  text-transform: uppercase;
+  border-radius: ${Theme.spacing[1]};
+
+  @media ${device.tablet} {
+    padding: ${Theme.spacing[2]};
+    max-width: 260px;
+  }
+
+  @media ${device.laptop} {
+    font-size: ${Theme.fonts.headerFour};
+    max-width: 350px;
+    width: 100%;
+  }
+
+  @media ${device.desktop} {
     padding: ${Theme.spacing[3]};
+  }
+
+  &:hover {
+    background: ${Theme.colors.primary};
     color: ${Theme.colors.darkBlue};
-    font-weight: bold;
-    font-size: ${Theme.fonts.normal};
-    text-transform: uppercase;
-    border-radius: ${Theme.spacing[1]};
-
-    @media ${device.tablet} {
-        padding: ${Theme.spacing[2]};
-        max-width: 260px;
-    }
-
-    @media ${device.laptop} {
-        font-size: ${Theme.fonts.headerFour};
-        max-width: 350px;
-        width: 100%;
-    }
-
-    @media ${device.desktop} {
-        padding: ${Theme.spacing[3]};
-    }
-
-    &:hover {
-        background: ${Theme.colors.primary};
-        color: ${Theme.colors.darkBlue};
-        };
+  }
 `
-
-
-
 
 // export const BannerContainer = styled(Box)(({ theme }) => ({
 //     display: 'flex',
@@ -152,7 +146,7 @@ export const BannerButton = styled.button`
 //       alignItems: 'center',
 //     },
 //   }));
-  
+
 //   export const BannerContent = styled(Box)(({ theme }) => ({
 //     display: 'flex',
 //     flexDirection: 'column',
@@ -172,7 +166,7 @@ export const BannerButton = styled.button`
 //       gap: '10px',
 //     },
 //   }));
-  
+
 //   export const BannerImage = styled('img')(({ src, theme }) => ({
 //     src: `url(${src})`,
 //     width: '600px',
@@ -186,7 +180,7 @@ export const BannerButton = styled.button`
 //       padding: '10px',
 //     },
 //   }));
-  
+
 //   export const BannerTitle = styled(Typography)(({ theme }) => ({
 //     '&::before': {
 //       content: '"THE WIKIPEDIA OF"',
@@ -211,7 +205,7 @@ export const BannerButton = styled.button`
 //       fontSize: 42,
 //     },
 //   }));
-  
+
 //   export const BannerDescription = styled(Typography)(({ theme }) => ({
 //     lineHeight: 1.25,
 //     letterSpacing: 1.25,
@@ -225,7 +219,7 @@ export const BannerButton = styled.button`
 //       padding: '15px 0',
 //     },
 //   }));
-  
+
 //   export const BannerShopButton = styled(Button)(({ theme }) => ({
 //     background: Colors.body_bg,
 //     '&:hover': {
@@ -243,4 +237,3 @@ export const BannerButton = styled.button`
 //       fontSize: 14,
 //     },
 //   }));
-  
