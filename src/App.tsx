03e2c0c200046a19fb3134586aa5f8a1854e8bad
FuuKowatty <miedzio.2000@gmail.com>
2023-04-26 15:55:14 +0200

    change routing options

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Home } from 'pages/Home'
-import { Container } from 'styles/StyledContainer'
-import { GlobalStyles } from 'styles/Global'
+import { Container } from 'styles/globalComponents'
+import { GlobalStyles } from 'styles/globalStyles'
 import { Navbar } from 'layouts/Navbar'
 import { AppProvider } from 'context'
-import { PokemonList } from 'pages/PokemonList'
+import { PokemonType } from 'pages/PokemonType/PokemonType'
+import { PokemonList } from 'pages/PokemonList/PokemonList'
+import { FetchError } from 'components/common/FetchErrors/FetchError'
+import { PokemonSearch } from 'pages/PokemonSearch/PokemonSearch'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export function App() {
@@ -15,7 +18,10 @@ export function App() {
           <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/pokemons' element={<PokemonList />} />
+            <Route path='/pokemons/all' element={<PokemonList />} />
+            <Route path='/pokemons/search' element={<PokemonSearch />} />
+            <Route path='/pokemons/type/:typeName' element={<PokemonType />} />
+            <Route path='*' element={<FetchError />} />
           </Routes>
         </Container>
       </AppProvider>
