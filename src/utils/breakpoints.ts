f6b76cea440f851dc1c483afe54c2041e2a795fa
FuuKowatty <miedzio.2000@gmail.com>
2023-04-26 15:54:56 +0200

    move breakpoints to utils

---
+++ src/utils/breakpoints.ts
@@ -0,0 +1,11 @@
+export const size = {
+  tablet: '620px',
+  laptop: '1024px',
+  desktop: '1440px',
+}
+
+export const device = {
+  tablet: `(min-width: ${size['tablet']})`,
+  laptop: `(min-width: ${size['laptop']})`,
+  desktop: `(min-width: ${size['desktop']})`,
+}
