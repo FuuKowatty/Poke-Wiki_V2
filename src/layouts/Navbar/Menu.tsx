ee1370beb7cc73183b719ca6293fd886430d8d79
FuuKowatty <miedzio.2000@gmail.com>
2023-05-21 19:58:07 +0200

    change names of file
    from MobileMenu to Menu because its more appropriate

---
+++ src/layouts/Navbar/Menu.tsx
@@ -0,0 +1,58 @@
+import { MenuContainer, MenuIconContainer, StyledLink } from './Navbar.styled'
+import { useViewport } from 'hooks/useViewport'
+import { AiFillHome } from 'react-icons/ai'
+import { MdCatchingPokemon, MdFavorite } from 'react-icons/md'
+import { GiStrawberry } from 'react-icons/gi'
+import { useLocation } from 'react-router-dom'
+
+const MenuItems = [
+  {
+    title: 'home',
+    path: '/',
+    icon: <AiFillHome />,
+  },
+  {
+    title: 'pokemons',
+    path: '/pokemons/all',
+    icon: <MdCatchingPokemon />,
+  },
+  {
+    title: 'berries',
+    path: '/berries/all',
+    icon: <GiStrawberry />,
+  },
+  {
+    title: 'favorites',
+    path: '/favorites',
+    icon: <MdFavorite />,
+  },
+]
+
+export function Menu() {
+  const pathname = useLocation().pathname
+  const { isTablet, isMobile } = useViewport()
+
+  const checkIsActive = (path: string) => {
+    // It returns for example /pokemons/all => /pokemons
+    const baseUrl = path.split('/').slice(0, -1).join('/')
+
+    // !baseUrl return for home page
+    if (!baseUrl) {
+      return pathname === path
+    } else {
+      return pathname.startsWith(baseUrl)
+    }
+  }
+
+  return (
+    <MenuContainer>
+      {MenuItems.map((item) => (
+        <StyledLink to={item.path} key={item.title}>
+          <MenuIconContainer isActive={checkIsActive(item.path)}>
+            {isTablet || isMobile ? item.icon : item.title.toUpperCase()}
+          </MenuIconContainer>
+        </StyledLink>
+      ))}
+    </MenuContainer>
+  )
+}
