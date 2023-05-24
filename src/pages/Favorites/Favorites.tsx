ea031eaca3153bfd0d78a3210bf87b0b17271bd9
FuuKowatty <miedzio.2000@gmail.com>
2023-05-24 12:32:43 +0200

    filter-refactor: moved favorite filters to Filters folder

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,56 +1,34 @@
-import {
-  ClearItemsButton,
-  FavoriteItemsCount,
-  FilterButton,
-  FilterFavoriteContainer,
-  TypeFiltersContainer,
-} from './Favorites.styled'
 import { favItem } from 'components/CardInterface/CardInterface'
 import { PokemonCard } from 'components/Card/PokemonCard/PokemonCard'
 import { BerryCard } from 'components/Card/BerryCard/BerryCard'
 import { GridContainer } from 'styles/globalComponents'
-import { useFavoriteContext } from 'context/FavoriteContext/FavoritesProvider'
+import { FavoriteFilters } from 'components/Filters/FavoriteFilters/FavoriteFilters'
 import { useState } from 'react'
 
-const filtersButtonText = ['all', 'pokemons', 'berries']
+
 
 export function Favorites() {
-  const { favorites, handleClearItems, favoriteItemsLimit } = useFavoriteContext()
+
   const [actualFilters, setActualFilters] = useState('all')
+  const [filteredFavorites, setfilteredFavorites] = useState<favItem[]>([])
 
-  const filteredFavorites =
-    actualFilters === 'all'
-      ? favorites
-      : favorites.filter((fav: favItem) => {
-          return actualFilters === 'berries' ? fav.type === 'berry' : fav.type === 'pokemon'
-        })
-
-  const handleFilter = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const target = event.target as HTMLButtonElement
-    const value = target.getAttribute('data-value') as string
-    setActualFilters(value)
+  const handleActualFilters = (buttonValue:string) => {
+    setActualFilters(buttonValue)
   }
+
+  const handleSelectFilters = (filteredType: favItem[]) => {
+    setfilteredFavorites(filteredType)
+  } 
+
+
 
   return (
     <>
-      <FilterFavoriteContainer>
-        <TypeFiltersContainer>
-          {filtersButtonText.map((text) => (
-            <FilterButton
-              key={text}
-              data-value={text}
-              onClick={handleFilter}
-              isActive={text === actualFilters}
-            >
-              {text}
-            </FilterButton>
-          ))}
-        </TypeFiltersContainer>
-        <ClearItemsButton onClick={handleClearItems}>Clear all</ClearItemsButton>
-        <FavoriteItemsCount>
-          {filteredFavorites.length} / {favoriteItemsLimit}
-        </FavoriteItemsCount>
-      </FilterFavoriteContainer>
+      <FavoriteFilters 
+        actualFilters={actualFilters} 
+        handleActualFilters={handleActualFilters} 
+        handleSelectFilters={handleSelectFilters}
+      />
       <GridContainer>
         {filteredFavorites.map((fav: favItem) => {
           const { name, type } = fav
